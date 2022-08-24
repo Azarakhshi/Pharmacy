4a671b7ed979ba34c3b00a9fd96ff0727329a51d
Amin Azarakhshi <azarakhshi.mohammadamin@gmail.com>
2022-08-24 10:55:15 +0430

    refactor prescriptions service

--- src/main/java/service/prescriptions/PrescriptionsService.java
+++ src/main/java/service/prescriptions/PrescriptionsService.java
@@ -5,7 +5,15 @@ import entity.Prescriptions;
 import java.sql.SQLException;
 
 public interface PrescriptionsService {
-    int findPrescriptionsId() throws SQLException;
+    /*
+    Prescriptions findPrescriptionsId() throws SQLException;*/
     void addDrugtoPrescriptions(Prescriptions prescriptions) throws SQLException;
+    boolean isPatientexist(String patientsname) throws SQLException;
+    void priceForEachitem(String patientsname)throws SQLException;
+    int totalPrice(String patientsname)throws SQLException;
+    void deleteprescription(String patientsname) throws  SQLException;
+    void editPrescriptionsDrug(Prescriptions newPrescriptions , Prescriptions oldPrescriptions) throws SQLException;
+    void showAllPrescriptions()throws SQLException;
+    void adminEdit(Prescriptions newPrescriptions, Prescriptions oldPrescriptions)throws SQLException;
 
 }
--- src/main/java/service/prescriptions/PrescriptionsServiceImpl.java
+++ src/main/java/service/prescriptions/PrescriptionsServiceImpl.java
@@ -5,20 +5,102 @@ import repository.prescriptions.PrescriptionsRepository;
 
 import java.sql.SQLException;
 
-public class PrescriptionsServiceImpl implements PrescriptionsService{
+public class PrescriptionsServiceImpl implements PrescriptionsService {
     private PrescriptionsRepository prescriptionsRepository;
 
     public PrescriptionsServiceImpl(PrescriptionsRepository prescriptionsRepository) {
         this.prescriptionsRepository = prescriptionsRepository;
     }
-
+/*
     @Override
-    public int findPrescriptionsId() throws SQLException {
-        return (prescriptionsRepository.lastPrescriptionsId() + 1);
-    }
+    public Prescriptions findPrescriptionsId() throws SQLException {
+        return (prescriptionsRepository.lastPrescriptionsId());
+    }*/
 
     @Override
     public void addDrugtoPrescriptions(Prescriptions prescriptions) throws SQLException {
         prescriptionsRepository.save(prescriptions);
+    }
+
+    @Override
+    public boolean isPatientexist(String patientsname) throws SQLException {
+        return prescriptionsRepository.isPatientexist(patientsname);
+    }
+
+    @Override
+    public void priceForEachitem(String patientsname) throws SQLException {
+        Prescriptions[] prescriptions;
+        prescriptions = prescriptionsRepository.priceForEachitem(patientsname);
+        while (true){
+            if(prescriptions.length == 0 || prescriptions[0]==null){
+                System.out.println("you have not prescription confirmed. please wait ");
+                break;
+            }else{
+                for (int i = 0 ; i < prescriptions.length ; i++){
+                    if(prescriptions[i] != null){
+                        System.out.println((i+1) + " _  " + prescriptions[i]);
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    public int totalPrice(String patientsname) throws SQLException {
+        int sum = 0;
+        Prescriptions[] prescriptions;
+        prescriptions = prescriptionsRepository.priceForEachitem(patientsname);
+        while (true){
+            if(prescriptions.length == 0 || prescriptions[0]==null){
+                System.out.println("you have not prescription confirmed. please wait ");
+                break;
+            }else{
+                for (int i = 0 ; i < prescriptions.length ; i++){
+                    if(prescriptions[i] != null){
+                         sum += prescriptions[i].getPrice() * prescriptions[i].getQuantity();
+                    }
+                }
+            }
+            break;
+        }
+        return sum;
+    }
+
+
+    @Override
+    public void deleteprescription(String patientsname) throws SQLException {
+        Prescriptions prescriptions = new Prescriptions();
+        prescriptions.setPatientsName(patientsname);
+        prescriptionsRepository.delete(prescriptions);
+    }
+
+    @Override
+    public void editPrescriptionsDrug(Prescriptions newPrescriptions , Prescriptions oldPrescriptions) throws SQLException {
+        prescriptionsRepository.edtit(newPrescriptions , oldPrescriptions);
+    }
+
+    @Override
+    public void showAllPrescriptions() throws SQLException {
+        Prescriptions[] prescriptions;
+        prescriptions = prescriptionsRepository.showAllPrescriptions();
+        while (true){
+            if(prescriptions.length == 0 || prescriptions[0]==null){
+                System.out.println("you have not prescription confirmed. please wait ");
+                break;
+            }else{
+                for (int i = 0 ; i < prescriptions.length ; i++){
+                    if(prescriptions[i] != null){
+                        System.out.println((i+1) + " _  " + prescriptions[i]);
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    public void adminEdit(Prescriptions newPrescriptions, Prescriptions oldPrescriptions) throws SQLException {
+        prescriptionsRepository.adminEdit(newPrescriptions , oldPrescriptions);
     }
 }
